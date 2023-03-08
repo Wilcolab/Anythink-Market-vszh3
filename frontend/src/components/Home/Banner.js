c28012d0d5bb305a5f0d87909fc79cd6e9569722
WilcoApp <hello@wilco.gg>
2023-03-08 03:28:31 +0000

    Hooks of Change

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import logo from '../../imgs/logo.png';
-import styled from 'styled-components';
+import React from "react";
+import logo from "../../imgs/logo.png";
 
 const Banner = () => {
-  const StyledDiv = styled.div`
-    background-color: ${(props) => props.theme.dark};
-  `;
   return (
-    <div className='banner text-white'>
-      <div className='container p-4 text-center'>
-        <img src={logo} alt='banner' />
-        <StyledDiv>
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
           <span>A place to </span>
-          <span id='get-part'>get</span>
+          <span id="get-part">get</span>
           <span> the cool stuff.</span>
-        </StyledDiv>
+        </div>
       </div>
     </div>
   );
