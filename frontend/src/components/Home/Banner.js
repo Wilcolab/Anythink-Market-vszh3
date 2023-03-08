5d18626b5ead50efc7ab3dd421f630bae9d42fb4
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-03-07 18:44:05 -0800

    Added a styled div to banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,16 +1,20 @@
-import React from "react";
-import logo from "../../imgs/logo.png";
+import React from 'react';
+import logo from '../../imgs/logo.png';
+import styled from 'styled-components';
 
 const Banner = () => {
+  const StyledDiv = styled.div`
+    background-color: ${(props) => props.theme.dark};
+  `;
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
+    <div className='banner text-white'>
+      <div className='container p-4 text-center'>
+        <img src={logo} alt='banner' />
+        <StyledDiv>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id='get-part'>get</span>
           <span> the cool stuff.</span>
-        </div>
+        </StyledDiv>
       </div>
     </div>
   );
